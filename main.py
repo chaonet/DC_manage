db9cb205cb43d7b4cc1e7436c2a37ea8dcc4c537
chaonet <xuchaorfc@gmail.com>
2016-09-25 20:56:39 +0800

    read login.xls

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 # coding:utf-8
 import re
 import telnetlib
+import xlrd
+import xlwt
 
 logs = []
 
+data = xlrd.open_workbook('login.xls') # 打开xls文件
+table = data.sheets()[0] # 打开第一张表
+nrows = table.nrows # 获取表的行数
+for i in range(nrows): # 循环逐行打印
+    if i == 0: # 跳过第一行
+        continue
+    ip_addre = table.row_values(i)[1] # 取第二列
+    print ip_addre
+    username = table.row_values(i)[2]
+    print username
+    password = table.row_values(i)[3]
+    print password
+
+
 # 登陆设备
 
-tn = telnetlib.Telnet('x.x.x.x')
-tn.write('test' + '\n')
+tn = telnetlib.Telnet(ip_addre)
+tn.write(str(username) + '\n')
 tn.read_until('Password:')
-tn.write('test' + '\n')
+tn.write(str(password) + '\n')
 echo = tn.read_until('>')
 print echo
 logs.append(echo)
@@ -39,4 +55,3 @@ with open(sysname_user+'.txt','a') as f:
 # 登出
 
 tn.close()
-
